1c807dede766cdd01d6cd2888f51e2a56d3d54f3
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-01 22:07:58 +0400

    Mantiksal Degerler ve Karsilastirma operatorleri

---
+++ Mantiksal Degerler ve Karsilastirma operatorleri.py
@@ -0,0 +1,16 @@
+#  Mantiksal Degerler ve Karsilastirma operatorleri
+
+# Boolean iki deger alir True ve False
+a=True # 0-dan farkli degerler
+a=False # sadece 0 olarsa
+
+a=None # none henuz deger atamadik anlamina gelir
+
+"""
+== esitlik
+!= esit degil
+> buyuktur
+< kucuktur
+>= buyuk esitdir
+<= kucuk esitdir
+"""
