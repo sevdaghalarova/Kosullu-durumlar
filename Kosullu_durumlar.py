6cce8de475ac604a63fe3f0a1bdbcc3ea338bdfe
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-01 22:12:49 +0400

    Kosullu durumlar.

---
+++ Kosullu_durumlar.py
@@ -0,0 +1,8 @@
+# Kosullu durumlar
+# if else
+yas=int(input("Yasinizi girin: ")) # kullanicidan yasini sorduk
+if yas>=18: # eger yasi 18 veya 18den buyukse
+    print(" Mekana hosgeldiniz") # bu yazi yazilacak
+else: # degilse
+    print("Mekana giremezsiniz...") # buradaki yazi ekrana bastirilacak
+
