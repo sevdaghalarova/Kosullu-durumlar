ac45b135095252719f0873ee1966952491fabb26
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-01 22:10:18 +0400

    Mantıksal bağlaçlar

---
+++ Mantiksal_baglaclar.py
@@ -0,0 +1,6 @@
+# Mantiksal baglaclar
+
+# and - Tum islemler True ise sonuc True doner
+# or - Islemlerden 1 tane True varsa True doner, hepsi False ise False doner
+# not - Bir islem True ise False yapar, False ise True yapar
+
